e5e2e69de27767343cc6eeb0dda65407db6a1002
Halimah Oladosu <halimah.oladosu@revelsystems.com>
2021-07-09 13:49:25 +0300

    update tesla package

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tesla-factory-package
+This is a practice project on software engineering basics. It is about building functionalities for electric cars at the imaginary Tesla car factory. 
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,15 +1,15 @@
 import pytest
-from tesla import fabric
+from tesla import factory
 
 def test_color():
-  teslaY = fabric.Tesla("Y", "White")
+  teslaY = factory.Tesla("Y", "White")
   assert teslaY.color == "White", "Count is wrong"
 
 def test_color():
-  teslaY = fabric.Tesla("Y", "White")
+  teslaY = factory.Tesla("Y", "White")
   assert teslaY.color == "White", "Count is wrong"
 
 def test_modelX():
-    modelx = fabric.ModelX("black")
+    modelx = factory.ModelX("black")
     modelx.unlock()
     assert modelx.open_doors() == "Doors opens towards roof"
