59a8b7c83e6d86ec1ebd84f514a9134e9008f74b
Halimah Oladosu <halimah.oladosu@revelsystems.com>
2021-07-09 15:00:53 +0300

    Update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tesla-factory-package",
+    name="tesla",
     version="0.0.1",
     author="Halimah Oladosu",
     author_email="haleemaholadosu@gmail.com",
